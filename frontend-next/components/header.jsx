1665ec071ea2bc4654bfab41c7f4644cc498952b
shahin1290 <s.patowary@yahoo.com>
2022-11-14 15:48:10 +0100

    finishes header styling

--- frontend-next/components/header.jsx
+++ frontend-next/components/header.jsx
@@ -4,14 +4,26 @@ const Header = () => {
   return (
     <header className="header">
       <nav className="flex flex-jc-sb flex-ai-c">
-        <a href="/" className="header__log">
+        <a href="/" className="header__logo">
           <img src="images/logo.svg" alt="logo" />
         </a>
 
-        <a href="#" className="header__menu">
+        <a href="#" className="header__menu hide-for-desktop">
           <span></span>
           <span></span>
           <span></span>
+        </a>
+
+        <div className="header__links hide-for-mobile">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+          <a href="#">Blog</a>
+          <a href="#">Careers</a>
+        </div>
+
+        <a href="#" className="button header__cta hide-for-mobile">
+          Request Invite
         </a>
       </nav>
     </header>
