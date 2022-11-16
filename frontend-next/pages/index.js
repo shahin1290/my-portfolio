c14debe9f3a80ef73f311990b928af6960e3038e
shahin1290 <s.patowary@yahoo.com>
2022-11-16 13:50:32 +0100

    finishes mobile styling

--- frontend-next/pages/index.js
+++ frontend-next/pages/index.js
@@ -1,12 +1,14 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import MainHome from "../components/MainHome";
+import ScrollUp from "../components/ScrollUp";
 
 export default function Home() {
   return (
     <>
       <Header />
       <MainHome />
+      <ScrollUp />
       <Footer />
     </>
   );
